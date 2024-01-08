9c24bbec303bffeb5cb645d1ce4257ecf05ffc41
Jyzdcs <123254210+Jyzdcs@users.noreply.github.com>
2024-01-08 15:52:39 +0100

    Add files via upload

---
+++ New version/main.js
@@ -0,0 +1,15 @@
+const redirectButton = document.getElementById("redirectButtonLvlOne");
+redirectButton.onclick = function (){
+    setTimeout(() => {
+        redirectButton.textContent = "Wait... 3s";
+        setTimeout(() => {
+            redirectButton.textContent = "Wait... 2s";
+            setTimeout(() => {
+                redirectButton.textContent = "Wait... 1s";
+                setTimeout(() => {
+                    location.href = "stage_1/index.html";
+                }, "1000");
+            }, "1000");
+        }, "1000");
+    }, "1000");
+}
